c6d679e33bbf8855b257c0b8f5d1d2eb6556d1cb
kawax <kawaxbiz@gmail.com>
2019-06-02 22:52:43 +0900

    test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,18 @@
 
 namespace Tests\Feature;
 
+use Mockery;
+use App\User;
 use Tests\TestCase;
+use App\Search\FilterQuery;
+use App\Search\SampleQuery;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Revolution\Google\SearchConsole\Facades\SearchConsole;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -15,6 +22,45 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testHome()
+    {
+        $user = factory(User::class)->create();
+
+        SearchConsole::shouldReceive('setAccessToken->listSites')->andReturn([]);
+
+        $response = $this->actingAs($user)->get('/home');
+
+        $response->assertStatus(200);
+    }
+
+    public function testSite()
+    {
+        $user = factory(User::class)->create();
+
+        SearchConsole::shouldReceive('setAccessToken->query')
+                     ->with('test', Mockery::type(SampleQuery::class))
+                     ->once()
+                     ->andReturn([]);
+
+        $response = $this->actingAs($user)->get('/site?url=test');
+
+        $response->assertStatus(200);
+    }
+
+    public function testFilter()
+    {
+        $user = factory(User::class)->create();
+
+        SearchConsole::shouldReceive('setAccessToken->query')
+                     ->with('test', FilterQuery::class)
+                     ->once()
+                     ->andReturn([]);
+
+        $response = $this->actingAs($user)->get('/filter?q=test&url=test');
 
         $response->assertStatus(200);
     }
